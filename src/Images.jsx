479bcfe0f0ff87c65eec6d02785d6bd2d97ac834
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-09-01 23:27:47 +0700

    change 4x4, fixing UI

--- src/Images.jsx
+++ src/Images.jsx
@@ -2,17 +2,13 @@ import Meme1 from './memes/memes1.jpg'
 import Meme4 from './memes/memes4.webp'
 import Meme5 from './memes/meme5.jpg'
 import Meme6 from './memes/meme6.jpg'
-import Meme8 from './memes/meme8.jpg'
-import Meme11 from './memes/meme11.jpg'
 import Meme12 from './memes/memes12.jpg'
 import Meme13 from './memes/memes13.jpg'
 import Meme14 from './memes/memes14.jpg'
-import Meme16 from './memes/memes16.jpg'
-import Meme17 from './memes/memes17.jpg'
 import Meme18 from './memes/memes18.jpg'
 
 
 
 
-const Images = [Meme1,Meme4,Meme5,Meme6,Meme8,Meme11,Meme12,Meme13,Meme14,Meme16,Meme17,Meme18]
+const Images = [Meme1,Meme4,Meme5,Meme6,Meme18,Meme14,Meme12,Meme13,]
 export default Images
